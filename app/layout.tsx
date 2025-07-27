5bdce0193c7a4eb4e5b0ff14ba34ec5130221e99
jhesid2345 <jhesid2345@gmail.com>
2025-07-26 21:13:04 -0400

    fix(instrumentos): :hammer: Correccion

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,7 @@ const isProd = process.env.NEXT_PUBLIC_ENV === 'production';
 const basePath = isProd ? process.env.NEXT_PUBLIC_BASE_PATH || '' : '';
 
 export const metadata = {
-  title: `CLN – Clazoni | Tienda de Instrumentos Musicales en Bolivia`,
+  title: `${process.env.JWT_SECRET} CLN – Clazoni | Tienda de Instrumentos Musicales en Bolivia`,
   description:
     'Descubre guitarras, teclados, baterías y más en CLN Clazoni. Tu tienda de instrumentos musicales en Bolivia con envíos a todo el país.',
   keywords: [
